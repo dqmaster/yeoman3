1ec9bfe3d8f8622ed506daacc4d7d306be3417ff
unknown <ma@AAL-MA.Admiral.local>
2015-05-18 17:02:00 -0400

    add to yeoman3

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name yeoman3App
+ * @description
+ * # yeoman3App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('yeoman3App', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
